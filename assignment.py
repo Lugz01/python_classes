b46c9ea65ef4c396a8605375bc34b9e028ec2f51
Lugz <delesaheed15@gmail.com>
2022-10-29 12:16:17 -0700

    Assignment and files updated

---
+++ assignment.py
@@ -0,0 +1,94 @@
+from email.errors import MalformedHeaderDefect
+from time import time
+
+
+name = "amir"
+# print(name)
+# check = name.istitle()
+# print(name.istitle())
+
+# if not name.istitle():
+#     capitalize_name = name.capitalize()
+#     print(capitalize_name)
+# else:
+#     print("name is already titled")
+# print(check)
+
+print(name*5)
+name="lande"
+print(name)
+
+# age =  21
+# print(name,"is",age)
+# print(f"{name} is {age}")
+# # name.rsplit
+# newName = "my name is saheed"
+# nnewName = newName.split()
+# nnewName.pop()
+# x = str(nnewName)
+# print(type(x))
+# # print(name)
+# nameOfPeople: list = ["arnold","tim","bold","luke"]
+# print(nameOfPeople.index("tima"))
+# nameOfPeople.pop()
+# print(nameOfPeople)
+# nameOfPeople.append("kola")
+# print(nameOfPeople)
+name= "KolAwole"
+dd = name.casefold()
+print(dd)
+# print(name)
+item = [2,3]
+print(item)
+
+for number in range(4,1000):
+    item.append(number)
+
+item.reverse()
+# print(item)
+gender = "male"
+ame = "ibrahim"
+amount = 45000
+
+# print(f"{ame} is {gender} and eligible to {amount}")
+
+# apc = int(input())
+# if apc is 100:        
+    # print("buhari is the winner")
+
+
+pp0# apc = int(input())
+# pdp = int(input())
+# Lp = int(input())
+# if apc == 90:
+#     print('buhari is the winner')
+# elif pdp == 90:
+#     print("obasanjo is the winner")
+# elif Lp == 90:
+#     print("obi is the winner")
+# else:
+#     print("election is inconclusive")
+
+
+# for num in int(range(0,10):)
+    # print("num")
+
+timz = lambda x,y:x*y
+# print(timz(3,7))
+
+def tim (x,y):
+    tim_Add = x+y
+    # print(tim_Add)
+    return
+# tim(3,7)
+list1=[2,5,8,1,0]
+# print(list1[3])
+
+print(list1[1:4])
+print(list1[:3])
+list1[4]=12
+print(list1)
+
+tuple1 = (5,7,2,90,4)
+print(tuple1)
+tuple1
